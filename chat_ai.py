1866cf46c26b64ec1773a7867cedb6404d9cf8a8
Tien.Tran <tientm@rakumo.vn>
2025-05-12 13:15:31 +0700

    add note

--- chat_ai.py
+++ chat_ai.py
@@ -4,10 +4,10 @@ with model.chat_session() as session:
     print('############################################')
     print('GPT4All Screen - Open')
     print('############################################')
-    print('Llama AI: How are you today? Can I help you?')
+    print('####### Llama AI: How are you today? Can I help you?')
 
     while True:
-        user_input = input("You: ")  # User input
+        user_input = input("####### You: ")  # User input
         
         if user_input.lower() == "exit":
             print("Bot: Goodbye! See you again.")
@@ -16,7 +16,7 @@ with model.chat_session() as session:
         # Bot phản hồi dựa trên câu hỏi của người dùng
         response = session.generate(user_input, max_tokens=1024)
         
-        print("Llama AI: ", response)  #
+        print("####### Llama AI: ", response)  #
 
     #print(model.generate("How can I run LLMs efficiently on my laptop?", max_tokens=1024))
     print('############################################')
